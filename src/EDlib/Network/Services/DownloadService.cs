481ef041a979c9598ed752b643146bfe18e4236f
Irongut <murray.dave@outlook.com>
2020-06-14 02:31:21 +0100

    added GetData() with cancel token #4

--- src/EDlib/Network/Services/DownloadService.cs
+++ src/EDlib/Network/Services/DownloadService.cs
@@ -2,11 +2,12 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EDlib.Network
 {
-    public sealed class DownloadService
+    internal sealed class DownloadService
     {
         private static readonly DownloadService instance = new DownloadService();
 
@@ -65,6 +66,52 @@ namespace EDlib.Network
                 // download data
                 var uri = new Uri(url);
                 HttpResponseMessage response = await client.GetAsync(uri).ConfigureAwait(false);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new APIException($"{response.StatusCode} - {response.ReasonPhrase}", (int)response.StatusCode);
+                }
+                else
+                {
+                    data = await HttpHelper.ReadContentAsync(response).ConfigureAwait(false);
+                    lastUpdated = DateTime.Now;
+                    // cache data
+                    cache.Add(dataKey, data, expiry);
+                    cache.Add(lastUpdatedKey, lastUpdated.ToString(), expiry);
+                }
+            }
+            return (data, lastUpdated);
+        }
+
+        public async Task<(string data, DateTime updated)> GetData(string url, string dataKey, string lastUpdatedKey, TimeSpan expiry, CancellationTokenSource cancelToken, bool ignoreCache = false)
+        {
+            string data;
+            DateTime lastUpdated;
+
+            if (!connectivity.IsConnected())
+            {
+                // no valid connectivity
+                if (cache.Exists(dataKey))
+                {
+                    // use cached data
+                    data = cache.Get(dataKey);
+                    lastUpdated = DateTime.Parse(cache.Get(lastUpdatedKey));
+                }
+                else
+                {
+                    throw new NoNetworkNoCacheException("No Internet available and no data cached.");
+                }
+            }
+            else if (!ignoreCache && cache.Exists(dataKey) && !cache.IsExpired(dataKey))
+            {
+                // use cached data
+                data = cache.Get(dataKey);
+                lastUpdated = DateTime.Parse(cache.Get(lastUpdatedKey));
+            }
+            else
+            {
+                // download data
+                var uri = new Uri(url);
+                HttpResponseMessage response = await client.GetAsync(uri, cancelToken.Token).ConfigureAwait(false);
                 if (!response.IsSuccessStatusCode)
                 {
                     throw new APIException($"{response.StatusCode} - {response.ReasonPhrase}", (int)response.StatusCode);
