b0387c1ba10a8ed4712e8bb62f2746aedc5c5017
Irongut <murray.dave@outlook.com>
2020-06-15 22:55:50 +0100

    added BGS Tick #7

---
+++ src/EDlib/BGS/Models/BgsTick.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace EDlib.BGS
+{
+    public class BgsTick
+    {
+        private DateTime _time;
+        [JsonProperty(PropertyName = "TIME")]
+        public DateTime Time
+        {
+            get { return _time; }
+            set
+            {
+                if (_time != value)
+                {
+                    _time = value;
+                    TimeString = value.ToString("g");
+                }
+            }
+        }
+
+        public string TimeString { get; internal set; }
+
+        public BgsTick()
+        {
+            TimeString = "Unknown";
+        }
+
+        public BgsTick(DateTime time)
+        {
+            Time = time;
+            TimeString = time.ToString("g");
+        }
+    }
+}
---
+++ src/EDlib/BGS/Services/BgsTickService.cs
@@ -0,0 +1,69 @@
+﻿using EDlib.Network;
+using EDlib.Platform;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EDlib.BGS
+{
+    public sealed class BgsTickService
+    {
+        private static readonly BgsTickService instance = new BgsTickService();
+
+        private static string agent;
+
+        private static ICacheService cache;
+
+        private static IConnectivityService connectivity;
+
+        private readonly string TickURL = "http://tick.phelbore.com/api/ticks?start={0}&end={1}";
+        private const string dataKey = "BgsTick";
+        private const string lastUpdatedKey = "BgsTickLastUpdated";
+
+        private List<BgsTick> bgsTicks;
+        private DateTime lastUpdated;
+
+        private BgsTickService()
+        {
+            bgsTicks = new List<BgsTick>();
+        }
+
+        public static BgsTickService Instance(string userAgent, ICacheService cacheService, IConnectivityService connectivityService)
+        {
+            agent = userAgent;
+            cache = cacheService;
+            connectivity = connectivityService;
+            return instance;
+        }
+
+        public async Task<(BgsTick tick, DateTime updated)> GetData(bool ignoreCache = false)
+        {
+            TimeSpan expiry = TimeSpan.FromHours(1);
+            if (bgsTicks == null || bgsTicks.Count < 7 || (lastUpdated + expiry < DateTime.Now))
+            {
+                string json;
+                string queryURL = String.Format(TickURL,
+                                         DateTime.UtcNow.Date.AddDays(-7).ToString("yyyy-MM-dd"),
+                                         DateTime.UtcNow.Date.ToString("yyyy-MM-dd"));
+
+                // download the json
+                DownloadService downloadService = DownloadService.Instance(agent, cache, connectivity);
+                (json, lastUpdated) = await downloadService.GetData(queryURL, dataKey, lastUpdatedKey, expiry, ignoreCache).ConfigureAwait(false);
+
+                // parse the data
+                bgsTicks = JsonConvert.DeserializeObject<List<BgsTick>>(json);
+            }
+
+            if (bgsTicks?.Any() == true)
+            {
+                return (bgsTicks.OrderByDescending(o => o.Time).First(), lastUpdated);
+            }
+            else
+            {
+                return (new BgsTick(), lastUpdated);
+            }
+        }
+    }
+}
