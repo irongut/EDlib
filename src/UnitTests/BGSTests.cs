16f59b4f2d30fdd6b0599794e7c6dee9ffd5f5b5
Irongut <murray.dave@outlook.com>
2020-06-17 22:33:03 +0100

    added BGS Tick tests #15

---
+++ src/UnitTests/BGSTests.cs
@@ -0,0 +1,47 @@
+﻿using EDlib.BGS;
+using EDlib.Mock.Platform;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BGSTests
+    {
+        [TestMethod]
+        public void UnknownTickTest()
+        {
+            BgsTick tick = new BgsTick();
+            Assert.AreEqual(tick.TimeString, "Unknown");
+        }
+
+        [TestMethod]
+        public void NewTickTest()
+        {
+            DateTime date = DateTime.Now;
+            BgsTick tick = new BgsTick(date);
+            Assert.AreEqual(tick.TimeString, date.ToString("g"));
+        }
+
+        [TestMethod]
+        public async Task LatestTickTest()
+        {
+            BgsTickService bgsService = BgsTickService.Instance("EDlib UnitTests", new EmptyCache(), new UnmeteredConnection());
+            (BgsTick tick, DateTime lastUpdated) = await bgsService.GetData().ConfigureAwait(false);
+            Assert.IsNotNull(tick);
+            Assert.IsTrue(lastUpdated > DateTime.Now.AddMinutes(-1));
+        }
+
+        [TestMethod]
+        public async Task MultipleTickTest()
+        {
+            BgsTickService bgsService = BgsTickService.Instance("EDlib UnitTests", new EmptyCache(), new UnmeteredConnection());
+            (List<BgsTick> ticks, DateTime lastUpdated) = await bgsService.GetData(7).ConfigureAwait(false);
+            Assert.IsNotNull(ticks);
+            Assert.IsTrue(lastUpdated > DateTime.Now.AddMinutes(-1));
+            Assert.IsTrue(ticks.Count > 1);
+        }
+    }
+}
