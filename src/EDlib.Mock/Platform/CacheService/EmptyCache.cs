9c84ce64b495bc78d4d7f8bfb4f41ca28e5a2b04
Irongut <murray.dave@outlook.com>
2020-06-17 22:22:54 +0100

    added mock classes for cache + connectivity #15

---
+++ src/EDlib.Mock/Platform/CacheService/EmptyCache.cs
@@ -0,0 +1,34 @@
+﻿using EDlib.Platform;
+using System;
+using System.Collections.Generic;
+
+namespace EDlib.Mock.Platform
+{
+    public class EmptyCache : ICacheService
+    {
+        public void Add(string key, string data, TimeSpan expireIn)
+        {
+            return;
+        }
+
+        public void EmptyAll()
+        {
+            return;
+        }
+
+        public void EmptyExpired()
+        {
+            return;
+        }
+
+        public bool Exists(string key) => false;
+
+        public string Get(string key) => string.Empty;
+
+        public DateTime? GetExpiration(string key) => DateTime.Now.AddDays(-1);
+
+        public IEnumerable<string> GetKeys(CacheState state = CacheState.Active) => null;
+
+        public bool IsExpired(string key) => true;
+    }
+}
