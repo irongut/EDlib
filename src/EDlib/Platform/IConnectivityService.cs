7f95706e086710e373a031c90a9cf2492fde9dcd
Irongut <murray.dave@outlook.com>
2020-06-22 00:46:59 +0100

    added docs for IConnectivityService #17

--- src/EDlib/Platform/IConnectivityService.cs
+++ src/EDlib/Platform/IConnectivityService.cs
@@ -1,9 +1,14 @@
 ﻿namespace EDlib.Platform
 {
+    /// <summary>Interface for a platform specific data connectivity service.</summary>
     public interface IConnectivityService
     {
+        /// <summary>Determines whether a network is available.</summary>
+        /// <returns><c>true</c> if a network connection is detected, else <c>false</c>.</returns>
         bool IsConnected();
 
+        /// <summary>Determines whether the network connection is metered.</summary>
+        /// <returns><c>true</c> if the connection is metered, else <c>false</c>.</returns>
         bool IsMetered();
     }
 }
