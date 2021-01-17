c2734ff122e503dd7691df6cc093527cda105824
Irongut <murray.dave@outlook.com>
2021-01-17 01:50:47 +0000

    improved EDSM Status tests #10

--- src/UnitTests/EdsmStatusTests.cs
+++ src/UnitTests/EdsmStatusTests.cs
@@ -17,6 +17,7 @@ namespace UnitTests
             Assert.IsTrue(eliteStatus.Status == -1);
             Assert.IsTrue(eliteStatus.Type.Contains("unknown", StringComparison.OrdinalIgnoreCase));
             Assert.IsTrue(eliteStatus.Message.Contains("unknown", StringComparison.OrdinalIgnoreCase));
+            Assert.IsTrue(eliteStatus.LastUpdated > DateTime.MinValue);
         }
 
         [TestMethod]
@@ -41,6 +42,8 @@ namespace UnitTests
             Assert.IsTrue(eliteStatus.Type.Contains("success", StringComparison.OrdinalIgnoreCase)
                        || eliteStatus.Type.Contains("warning", StringComparison.OrdinalIgnoreCase)
                        || eliteStatus.Type.Contains("danger", StringComparison.OrdinalIgnoreCase));
+            Assert.IsFalse(string.IsNullOrWhiteSpace(eliteStatus.Message));
+            Assert.IsTrue(eliteStatus.LastUpdated > DateTime.MinValue);
         }
     }
 }
