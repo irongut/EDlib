1b0a587a21679c7652c7a3c5380c00b1de6e502c
irongut <murray.dave@outlook.com>
2021-05-21 22:38:26 +0100

    updated Exceptions docs #70

--- src/EDlib/Exceptions/APIException.cs
+++ src/EDlib/Exceptions/APIException.cs
@@ -9,34 +9,25 @@ namespace EDlib
     [Serializable]
     public class APIException : Exception
     {
-        /// <summary>Gets the error code from the API.</summary>
-        /// <value>The error code.</value>
+        /// <summary>The error code from the API if available.</summary>
         public int? StatusCode { get; }
 
         /// <summary>Initializes a new instance of the <see cref="APIException" /> class.</summary>
-        public APIException()
-        {
-        }
+        public APIException() { }
 
         /// <summary>Initializes a new instance of the <see cref="APIException" /> class with a specified error message.</summary>
         /// <param name="message">The message that describes the error.</param>
-        public APIException(string message) : base(message)
-        {
-        }
+        public APIException(string message) : base(message) { }
 
         /// <summary>Initializes a new instance of the <see cref="APIException" /> class with a specified error message and a reference to the inner exception that is the cause of this exception.</summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
-        public APIException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        public APIException(string message, Exception innerException) : base(message, innerException) { }
 
         /// <summary>Initializes a new instance of the <see cref="APIException" /> class with serialised data.</summary>
         /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo">SerializationInfo</see> that holds the serialized object data about the exception being thrown.</param>
         /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext">StreamingContext</see> that contains contextual information about the source or destination.</param>
-        protected APIException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+        protected APIException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
         /// <summary>Initializes a new instance of the <see cref="APIException" /> class with a specified error message and error code.</summary>
         /// <param name="message">The message that describes the error.</param>
