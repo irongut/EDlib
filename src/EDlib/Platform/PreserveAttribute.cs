2099135243cbe029a0ee2522188541a86805b497
irongut <murray.dave@outlook.com>
2021-05-21 23:41:45 +0100

    updated Platform docs #70

--- src/EDlib/Platform/PreserveAttribute.cs
+++ src/EDlib/Platform/PreserveAttribute.cs
@@ -5,8 +5,8 @@ namespace EDlib.Platform
 {
 	/// <summary>
 	///   <para>Linker control attribute.</para>
-	///   <para>To preserve the whole type, use the syntax <c>[Preserve (AllMembers = true)]</c> on the type.
-	/// To preserve a member if the containing type was preserved, use <c>[Preserve (Conditional=true)]</c> on the member.</para>
+	///   <para>To preserve the whole type, use the syntax <c>[Preserve (AllMembers = true)]</c> on the type.<br/>
+	///			To preserve a member if the containing type was preserved, use <c>[Preserve (Conditional=true)]</c> on the member.</para>
 	///   <para>See: https://docs.microsoft.com/en-gb/xamarin/ios/deploy-test/linker </para>
 	/// </summary>
 	[AttributeUsage(AttributeTargets.All)]
@@ -20,8 +20,8 @@ namespace EDlib.Platform
 		public bool Conditional;
 
 		/// <summary>Preserve attribute constructor.</summary>
-		/// <param name="allMembers">Set <c>true</c> when preserving the whole type.</param>
-		/// <param name="conditional">Set <c>true</c> when preserving an individual type member.</param>
+		/// <param name="allMembers">Set <c>true</c> to preserve the whole type.</param>
+		/// <param name="conditional">Set <c>true</c> to preserve an individual type member.</param>
 		public PreserveAttribute(bool allMembers, bool conditional)
 		{
 			AllMembers = allMembers;
@@ -29,8 +29,6 @@ namespace EDlib.Platform
 		}
 
 		/// <summary>Preserve attribute default constructor.</summary>
-		public PreserveAttribute()
-		{
-		}
+		public PreserveAttribute() { }
 	}
 }
