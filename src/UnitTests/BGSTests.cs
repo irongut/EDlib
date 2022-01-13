ae13b8eddbba3ef6359138bfa4496f8588592f47
irongut <murray.dave@outlook.com>
2022-01-13 00:16:10 +0000

    mocked BGS tests

--- src/UnitTests/BGSTests.cs
+++ src/UnitTests/BGSTests.cs
@@ -1,7 +1,8 @@
 ﻿using EDlib.BGS;
 using EDlib.Network;
-using EDlib.Mock.Platform;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -29,20 +30,47 @@ namespace UnitTests
         [TestMethod]
         public async Task LatestTickTest()
         {
-            BgsTickService bgsService = BgsTickService.Instance(DownloadService.Instance("EDlib UnitTests", new UnmeteredConnection()));
+            DateTime timestamp = DateTime.Now - TimeSpan.FromHours(24);
+            string tickData = JsonConvert.SerializeObject(new List<BgsTick>() { new BgsTick(timestamp) });
+
+            Mock<IDownloadService> mockDownloadService = new();
+            mockDownloadService.Setup(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()).Result).Returns((tickData, DateTime.Now));
+
+            BgsTickService bgsService = BgsTickService.Instance(mockDownloadService.Object);
             (BgsTick tick, DateTime lastUpdated) = await bgsService.GetData().ConfigureAwait(false);
+
             Assert.IsNotNull(tick);
+            Assert.AreEqual(timestamp, tick.Time);
+            Assert.AreEqual(timestamp.ToString("g"), tick.TimeString);
             Assert.IsTrue(lastUpdated > DateTime.Now.AddMinutes(-1));
+            mockDownloadService.Verify(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()), Times.Once());
         }
 
         [TestMethod]
         public async Task MultipleTickTest()
         {
-            BgsTickService bgsService = BgsTickService.Instance(DownloadService.Instance("EDlib UnitTests", new UnmeteredConnection()));
+            DateTime timestamp = DateTime.Now - TimeSpan.FromHours(24);
+            string tickData = JsonConvert.SerializeObject(new List<BgsTick>()
+            {
+                new BgsTick(timestamp),
+                new BgsTick(timestamp - TimeSpan.FromDays(1)),
+                new BgsTick(timestamp - TimeSpan.FromDays(2)),
+                new BgsTick(timestamp - TimeSpan.FromDays(3)),
+                new BgsTick(timestamp - TimeSpan.FromDays(4))
+            });
+
+            Mock<IDownloadService> mockDownloadService = new();
+            mockDownloadService.Setup(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()).Result).Returns((tickData, DateTime.Now));
+
+            BgsTickService bgsService = BgsTickService.Instance(mockDownloadService.Object);
             (List<BgsTick> ticks, DateTime lastUpdated) = await bgsService.GetData(5).ConfigureAwait(false);
+
             Assert.IsNotNull(ticks);
-            Assert.IsTrue(lastUpdated > DateTime.Now.AddMinutes(-1));
             Assert.IsTrue(ticks.Count == 5);
+            Assert.AreEqual(timestamp, ticks[0].Time);
+            Assert.AreEqual(timestamp.ToString("g"), ticks[0].TimeString);
+            Assert.IsTrue(lastUpdated > DateTime.Now.AddMinutes(-1));
+            mockDownloadService.Verify(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()), Times.Once());
         }
     }
 }
