22438773ed07f93d16188b45bc5a81d1ae741d8c
irongut <murray.dave@outlook.com>
2022-01-22 00:16:52 +0000

    improved EdsmService tests

--- src/UnitTests/EDSM/EdsmServiceTests.cs
+++ src/UnitTests/EDSM/EdsmServiceTests.cs
@@ -1,8 +1,9 @@
 ﻿using EDlib.EDSM;
-using EDlib.Mock.Platform;
 using EDlib.Network;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace UnitTests
@@ -11,12 +12,42 @@ namespace UnitTests
     public class EdsmServiceTests
     {
         [TestMethod]
-        public async Task GetDataTest()
+        public async Task EdsmService_Get_NoParameters_Test()
         {
-            EdsmService edsmService = EdsmService.Instance(DownloadService.Instance("EDlib UnitTests", new UnmeteredConnection()));
-            (string json, DateTime lastUpdated) = await edsmService.GetData("api-status-v1/elite-server", null, new DownloadOptions()).ConfigureAwait(false);
-            Assert.IsFalse(string.IsNullOrWhiteSpace(json));
-            Assert.IsTrue(lastUpdated > DateTime.MinValue);
+            DateTime timestamp = DateTime.Now;
+            const string edsmData = "edsm-data";
+
+            Mock<IDownloadService> mockDownloadService = new();
+            mockDownloadService.Setup(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()).Result).Returns((edsmData, timestamp));
+
+            EdsmService edsmService = EdsmService.Instance(mockDownloadService.Object);
+            (string data, DateTime lastUpdated) = await edsmService.GetData("method", null, new DownloadOptions()).ConfigureAwait(false);
+
+            Assert.AreEqual(edsmData, data);
+            Assert.AreEqual(timestamp, lastUpdated);
+
+            mockDownloadService.Verify(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task EdsmService_Get_Test()
+        {
+            DateTime timestamp = DateTime.Now;
+            const string edsmData = "edsm-data";
+            Dictionary<string, string> edsmParams = new();
+            edsmParams.Add("key1", "value1");
+            edsmParams.Add("key2", "value2");
+
+            Mock<IDownloadService> mockDownloadService = new();
+            mockDownloadService.Setup(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()).Result).Returns((edsmData, timestamp));
+
+            EdsmService edsmService = EdsmService.Instance(mockDownloadService.Object);
+            (string data, DateTime lastUpdated) = await edsmService.GetData("method", edsmParams, new DownloadOptions()).ConfigureAwait(false);
+
+            Assert.AreEqual(edsmData, data);
+            Assert.AreEqual(timestamp, lastUpdated);
+
+            mockDownloadService.Verify(x => x.GetData(It.IsAny<string>(), It.IsAny<DownloadOptions>()), Times.Once());
         }
     }
 }
